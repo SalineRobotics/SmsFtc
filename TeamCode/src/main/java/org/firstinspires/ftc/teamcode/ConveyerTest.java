0d9f4dfb24a38d7b8ac0564136dde7efe1c01d46
Jeff Fansler <JeffF@FanzooTechnology.com>
2018-05-01 20:00:35 -0400

    working on new bot code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConveyerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConveyerTest.java
@@ -7,6 +7,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @TeleOp
 public class ConveyerTest extends LinearOpMode {
@@ -33,18 +35,55 @@ public class ConveyerTest extends LinearOpMode {
 
         DcMotor elevator = hardwareMap.dcMotor.get("elevator");
         DcMotor dump = hardwareMap.dcMotor.get("dump");
-        elevator.setDirection(DcMotorSimple.Direction.FORWARD);
-        dump.setDirection(DcMotorSimple.Direction.REVERSE);
         elevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         dump.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         int elevatorPosition = 0;
         int elevatorStatus = 0;
+
+        DcMotor relicMotor = hardwareMap.dcMotor.get("relicMotor");
+        relicMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        boolean relicArmPosition = false;
+        double relicArmPower = 0;
+        Servo relicExtend = hardwareMap.servo.get("relicExtend");
+        Servo relicHand = hardwareMap.servo.get("relicHand");
+        double          clawOffset      = 0;
+        double          clawOffset2     = 0;
+        final double    CLAW_SPEED      = 0.08 ;
+
+        Date stamp = new Date();
+
 
         boolean position1 = false;
         boolean position2 = false;
         waitForStart();
 
         while (opModeIsActive()) {
+            Date loopDate = new Date();
+            long diff = loopDate.getTime() - stamp.getTime();
+
+            if (diff >= 50) {
+                stamp = new Date();
+                if (gamepad2.dpad_up) {
+                    clawOffset += CLAW_SPEED;
+                } else if (gamepad2.dpad_down) {
+                    clawOffset -= CLAW_SPEED;
+                }
+
+                if (gamepad2.dpad_left) {
+                    clawOffset2 += CLAW_SPEED;
+                } else if (gamepad2.dpad_right) {
+                    clawOffset2 -= CLAW_SPEED;
+                }
+            }
+
+            clawOffset = Range.clip(clawOffset, -0.5, 0.5);
+            clawOffset2 = Range.clip(clawOffset2, -0.5, 0.5);
+            relicExtend.setPosition(0.5 + clawOffset2);
+            relicHand.setPosition(0.5 + clawOffset);
+
+            relicArmPower = gamepad2.right_stick_y;
+            relicMotor.setPower(Range.clip(relicArmPower, -0.5, 0.5));
+
 
             leftPower = Range.clip(gamepad1.left_stick_y, -0.5, 0.5);
             rightPower = Range.clip(gamepad1.right_stick_y, -0.5, 0.5);
@@ -107,7 +146,11 @@ public class ConveyerTest extends LinearOpMode {
 
 
 
-
+            telemetry.addData("Glyph Lift Status:", elevatorStatus);
+            telemetry.addData("Glyph Lift Position:", elevatorPosition);
+            telemetry.addData("Belt Power:", beltPower);
+            telemetry.addData("Left Drive:", leftDrive);
+            telemetry.addData("Right Drive:", rightDrive);
             telemetry.update();
         }
 
